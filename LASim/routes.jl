e47070ecb2a61e8ec5ecd81f87cd56d078a8e60a
Graham Stark <graham.stark@virtual-worlds.biz>
2024-03-14 07:35:48 +0000

    local running without Apache

--- LASim/routes.jl
+++ LASim/routes.jl
@@ -1,7 +1,29 @@
 using Genie.Router
 
 route("/run", LASim.run, method=POST )
+
 route("/reset", LASim.reset, method=POST )
+
+route("/addincome/:n", method = POST) do 
+  n::Int = parse(Int, payload(:n))
+  LASim.addincome( n )
+end
+
+route("/delincome/:n", method = POST ) do
+  n::Int = parse(Int, payload(:n))
+  LASim.delincome( n )
+end
+
+route("/addcapital/:n", method = POST ) do 
+  n::Int = parse(Int, payload(:n))
+  LASim.addcapital( n )
+end
+
+route("/delcapital/:n", method = POST ) do 
+  n::Int = parse(Int,  payload(:n))
+  LASim.delcapital( n )
+end
+
 route("/") do
-  serve_static_file("welcome.html")
+  serve_static_file("indexloc.html")
 end
