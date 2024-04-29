12e35087fdcbbe079ed9fbc723db1f29f9ed289d
Graham Stark <graham.stark@virtual-worlds.biz>
2024-04-29 10:01:03 +0100

    crosstab labelled in xl.

--- LASim/lib/exporter.jl
+++ LASim/lib/exporter.jl
@@ -13,20 +13,21 @@ const PRE_POST = Dict(
 function insert_sheet!( xf, typ, bd, prepost, df :: DataFrame )
     @show "adding sheet $typ $bd $prepost"
     sheet = XLSX.addsheet!( xf )
-    sheetname = "$typ $bd $prepost"
-    caption = "$(TYP_LABELS[typ]), broken down by $bd, $(PRE_POST[prepost])"
+    sheetname = "$typ $prepost"
     XLSX.rename!( sheet, sheetname )
     nrows, ncols = size(df)
     row = 1
-    sheet["A$row"] = caption
-    enum_to_string!( df ) # TRANSLate enums tp strings; xslx doesn't do enums
-    row += 2
-    XLSX.writetable!( sheet, df; anchor_cell=XLSX.CellRef("A$row"))
-    row += nrows+1
-    sheet["A$row"] = caption
-    row += 2
-    XLSX.writetable!( sheet, df; anchor_cell=XLSX.CellRef("A$row"))
-    
+    for bd in LegalAidOutput.LA_TARGETS
+        caption = "$(TYP_LABELS[typ]), broken down by $bd, $(PRE_POST[prepost])"
+        sheet["A$row"] = caption
+        enum_to_string!( df ) # TRANSLate enums tp strings; xslx doesn't do enums
+        row += 2
+        XLSX.writetable!( sheet, df; anchor_cell=XLSX.CellRef("A$row"))
+        row += nrows+2
+        sheet["A$row"] = caption
+        row += 2
+        XLSX.writetable!( sheet, df; anchor_cell=XLSX.CellRef("A$row"))
+    end
 end
 
 function export_xlsx( results :: LegalOutput )::String 
@@ -42,13 +43,13 @@ function export_xlsx( results :: LegalOutput )::String
                 results.crosstab_pers[1]);
                 anchor_cell=XLSX.CellRef("A3"))  
            # sheetname="Person Crosstab")
-        for t in LegalAidOutput.LA_TARGETS
-            insert_sheet!( xf, "Elig", t, "Pre", results.breakdown_pers[1][t])
-            insert_sheet!( xf, "Elig", t, "Post", results.breakdown_pers[2][t])
-            insert_sheet!( xf, "Cases", t, "Pre", results.cases_pers[1][t])
-            insert_sheet!( xf, "Cases", t, "Post", results.cases_pers[2][t])
-            insert_sheet!( xf, "Costs", t, "Pre", results.costs_pers[1][t])
-            insert_sheet!( xf, "Costs", t, "Post", results.costs_pers[2][t])
+        
+            insert_sheet!( xf, "Elig", "Pre", results.breakdown_pers[1][t])
+            insert_sheet!( xf, "Elig", "Post", results.breakdown_pers[2][t])
+            insert_sheet!( xf, "Cases", "Pre", results.cases_pers[1][t])
+            insert_sheet!( xf, "Cases", "Post", results.cases_pers[2][t])
+            insert_sheet!( xf, "Costs", "Pre", results.costs_pers[1][t])
+            insert_sheet!( xf, "Costs", "Post", results.costs_pers[2][t])
         end # for 
     end # xlsx do
     return urlname
