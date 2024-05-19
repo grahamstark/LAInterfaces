d2d4d609a5aa5818af99c05bc506f61f9686efe2
Graham Stark <graham.stark@virtual-worlds.biz>
2024-05-19 09:06:02 +0100

    multiple cleanups

---
+++ LASim/lib/utils.jl
@@ -0,0 +1,12 @@
+function pix( incs :: ::IncludedItems )
+    p = sort!(collect(incs).included))
+    println("INCLUDED")
+    for i in p
+      println(i)
+    end
+    p = sort!(collect(incs).deducted))
+    println("DEDUCTED")
+    for i in p
+      println(i)
+    end
+end
