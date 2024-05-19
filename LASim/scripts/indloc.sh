2a8e151732ed4b0d309e870b84d9949a65f4f64e
Graham Stark <graham.stark@virtual-worlds.biz>
2024-05-19 23:24:48 +0100

    multiple cleanups

--- LASim/scripts/indloc.sh
+++ LASim/scripts/indloc.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-/bin/cp -f web/index.html web/indexloc.html
-/usr/bin/vim -e web/indexloc.html < scripts/indexloc.vim
-/bin/cp -f web/index-session.html web/indexloc-session.html
-/usr/bin/vim -e web/indexloc-session.html < scripts/indexloc.vim
-
-
+/bin/cp -f web/index-apache.html web/index.html
+/usr/bin/vim -e web/index.html < scripts/indexloc.vim
